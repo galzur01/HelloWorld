4c59b619a9b9651e477df8043e2ea7aaaa2bcc5a
galzur <galzur01@gmail.com>
2024-01-21 19:09:34 +0200

    Added num variable

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         int num = 0;
+        System.out.println(num + 2);
 	// write your code here
     }
 }
